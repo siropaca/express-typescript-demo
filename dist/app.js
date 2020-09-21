d3d975708ef0721e6cbb703874dcf9a031f90e0c
siropaca <ryhshr3@gmail.com>
2020-09-21 18:35:21 +0900

    distを追加

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+var port = 5000;
+app.get("/", function (req, res) {
+    res.send("Hello World!!");
+});
+app.listen(port, function () {
+    console.log("Example app listening at http://localhost:" + port);
+});
